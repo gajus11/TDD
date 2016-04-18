1f3ae95318555c2150d3dd6e349ce3906a218835
Łukasz Gajownik <lukasz@gajownik.com>
2016-04-18 20:19:08 +0200

    End of chapter 5

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from lists.models import Item
 
-# Create your views here.
 def home_page(request):
-    context = {
-        'new_item_text' : request.POST.get('item_text', ''),
-    }
-    return render(request, 'home.html', context)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
