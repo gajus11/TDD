987b4a3b5b7560eab64fa99eeaf4b2b7a669a34b
lgajownik <gajus11@gmail.com>
2016-04-20 15:57:55 +0200

    Layout and styling FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -90,5 +90,27 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
 
+    def test_layout_and_styling(self):
+        # Home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Input box centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # New list is centered too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
