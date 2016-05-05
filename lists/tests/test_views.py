198561be16f77310ca6900c68f36f11376bd6935
lgajownik <gajus11@gmail.com>
2016-05-05 14:55:24 +0200

    Validate unique

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,6 +4,8 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.utils.html import escape
 
+from unittest import skip
+
 from lists.models import Item, List
 from lists.views import home_page
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
@@ -162,3 +164,17 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create()
+        response = self.client.post(
+            '/lists/%d' % (list1.id,),
+            data={'text' : 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
