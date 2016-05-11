723fdc56564fc2605600eb824203992664b93d8c
lgajownik <gajus11@gmail.com>
2016-05-11 16:31:00 +0200

    Fix

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def persona_login(request):
+    authenticate(assertion=request.POST['assertion'])
+    return HttpResponse()
