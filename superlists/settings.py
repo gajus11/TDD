cff4b78c71b242b83bb6cdb0790d52b30e2a1a35
lgajownik <gajus11@gmail.com>
2016-05-11 17:45:13 +0200

    Fix

--- superlists/settings.py
+++ superlists/settings.py
@@ -149,6 +149,6 @@ LOGGING = {
 
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
+    # 'django_jenkins.tasks.run_pyflakes',
     'django_jenkins.tasks.run_pylint',
 )
